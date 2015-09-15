00b9951a929645b0aae78b17f0e3d9474b3b117a
Manuel Bonilla <josemanuelbonilla@gmail.com>
2015-09-15 11:08:23 +0200

    Going back to an old version of phobic_camera code

--- src/cilinder_fitting.cpp
+++ src/cilinder_fitting.cpp
@@ -1,17 +1,18 @@
-
-#include <phobic_camera.h>
+#include "phobic_camera.h"
 
 int main(int argc, char** argv)
 {
-
-  ros::init(argc, argv, "PhobicDemo_CylinderFittingInfo");
+  //std::cout<<"Sono nel main"<<std::endl<<std::flush;
+  ros::init(argc, argv, "Phobic_whife_scene");
 
   ros::Subscriber reader;
-
+  // std::cout<<"Sono nel main prima di aver creato ogetto classe"<<std::endl;
+  
   ros::NodeHandle nodeH;
-  phobic_scene phobic_scene_local(nodeH, true);
+  phobic_scene phobic_scene_local(nodeH, true); 
+  // std::cout<<"Sono nel main dopo aver creato ogetto classe"<<std::endl;
 
-  double spin_rate = 10;
+  double spin_rate;
   ros::param::get("~spin_rate",spin_rate);
   ROS_INFO( "Spin Rate %lf", spin_rate);
 
@@ -20,16 +21,20 @@ int main(int argc, char** argv)
   ROS_INFO( "camera_topic %s", camera_topic.c_str());
 
   reader = nodeH.subscribe(nodeH.resolveName(camera_topic), 1, &phobic_scene::pointcloudCallback, &phobic_scene_local);
+  //std::cout<<"Sono nel main dopo aver letto"<<std::endl;
 
-  ros::Rate loop_rate( spin_rate );
+  
+  ros::Rate loop_rate( spin_rate ); // 5Hz
 
   while (ros::ok())
-    {
+  {
 
-      loop_rate.sleep();
-      ros::spinOnce();
+    loop_rate.sleep();
+    ros::spinOnce();
 
-    }
+  }
+  // ros::spin();
   return 0;
 
+  
 }
