e0dcab1b78d08219cd667bef1f46674264e83b09
DanyResasco <daniela.resasco@gmail.com+>
2016-01-13 17:04:54 +0100

    modify the color of the ball

--- src/ball_node.cpp
+++ src/ball_node.cpp
@@ -210,7 +210,10 @@
     marker.scale.x = 0.2;
     marker.scale.y = 0.2;
     marker.scale.z = 0.2;
-    marker.color.a = 1.0;
+    marker.color.a = 0.01; 
+    marker.color.r = 0.1;
+    marker.color.g = 0.0;
+    marker.color.b = 0.0;
     marker.pose.orientation.w = 1.0;
     marker.pose.position.x = pos_ball(0);
     marker.pose.position.y = pos_ball(1);
